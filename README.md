e0099add75daeb787cbf00ec2932a4b58eea3b78
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-09-09 20:15:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# snoutsaver
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/snoutsaver/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.snoutsaver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
