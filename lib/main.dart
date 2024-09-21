73a182942bec03d95b67cfbf78d7b99459b0a8b8
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-09-21 16:36:39 +0700

    feat(firebase): connecting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:snoutsaver/firebase_options.dart';
 import 'package:snoutsaver/pages/home_page.dart';
 // import 'package:snoutsaver/pages/signin_page.dart';
 import 'package:snoutsaver/pages/welcome_page.dart';
@@ -7,8 +9,12 @@ import 'package:snoutsaver/pages/welcome_page.dart';
 // import 'package:snoutsaver/pages/splash_page.dart';
 //import 'package:snoutsaver/pages/profile_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
